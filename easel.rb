da0d7f582b6e40c79a69ce44de74c6d6f87c4f8f
Lee Bergstrand <leemacboy@gmail.com>
2017-08-21 17:53:18 -0400

    Added homepage, head (latest source code if download fails), and end test.

--- easel.rb
+++ easel.rb
@@ -1,10 +1,12 @@
 class Easel < Formula
   desc 'Sequence analysis library used by Eddy/Rivas lab code'
-  homepage ''
+  homepage 'https://github.com/EddyRivasLab/easel'
 
   url 'https://github.com/EddyRivasLab/easel/archive/Bio-Easel-0.05.tar.gz'
   sha256 '5846b75e02fbbc3a01df8591e55207b1e7654072ad29679a78004c00ba05a65d'
+  head 'https://github.com/EddyRivasLab/easel.git'
 
+  option 'without-test', 'Skip build-time tests (not recommended)'
   depends_on 'autoconf' => :build
 
   def install
@@ -13,5 +15,9 @@ class Easel < Formula
     system 'make'
     system 'make', 'check' if build.with? 'test'
     system 'make', 'install'
+  end
+
+  test do
+    system 'esl-afetch', '-h'
   end
 end
