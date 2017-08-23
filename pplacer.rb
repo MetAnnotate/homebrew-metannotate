687114c2829149d4413a6081409578f1b26a1c90
Lee Bergstrand <leemacboy@gmail.com>
2017-08-23 15:11:54 -0400

    Prototype pplacer formula.

--- pplacer.rb
+++ pplacer.rb
@@ -1,37 +1,25 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook.html
-#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Pplacer < Formula
-  desc "Phylogenetic placement and downstream analysis"
-  homepage "http://matsen.fredhutch.org/pplacer/"
-  url "https://github.com/matsen/pplacer/archive/v1.1.alpha19.tar.gz"
-  sha256 "0e74f3bdaf996c6e54695e23bc2180877f2de0fadc80f0fe9d066aa445b3347c"
+  desc 'Phylogenetic placement and downstream analysis'
+  homepage 'http://matsen.fredhutch.org/pplacer/'
+  url 'https://github.com/matsen/pplacer/archive/v1.1.alpha19.tar.gz'
+  sha256 '0e74f3bdaf996c6e54695e23bc2180877f2de0fadc80f0fe9d066aa445b3347c'
 
-  # depends_on "cmake" => :build
+  depends_on 'camlp4' => :build
+  depends_on 'gawk' => :build
+  depends_on 'gsl' => :build
+  depends_on 'sqlite' => :build
+  depends_on 'm4' => :build
+  depends_on 'ocaml' => :build
+  depends_on 'opam' => :build
+  depends_on 'pkg-config' => :build
+  depends_on 'aspcud' => :build
+
+  depends_on :x11
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
 
-    # Remove unrecognized options if warned by configure
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
-    system "make", "install" # if this fails, try separate make/make install steps
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test pplacer`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
   end
 end
